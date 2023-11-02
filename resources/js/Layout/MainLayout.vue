ee83c0c416f184a73bec9823ca3b1f9f3242a2ab
SeeWenJie <149643512+SeeWenJie@users.noreply.github.com>
2023-11-02 23:20:15 +0800

    Add files via upload

---
+++ resources/js/Layout/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+    <Link href="/listing">Listings</Link>&nbsp;
+    <Link href="/listing/create">New Listing</Link>
+    
+    
+    <div v-if="flashSuccess" class="success">
+        {{flashSuccess}}
+    </div>
+    
+
+    <slot>Default</slot>
+</template>
+
+
+<script setup>
+    import {ref, computed } from 'vue'
+    import { Link, usePage } from '@inertiajs/vue3'
+
+    const x=ref(0)
+    const y=computed(()=>x.value*2)
+
+    //page.props.value.flash.success
+    const page=usePage()
+    const flashSuccess = computed(
+  () => page.props.flash.success,
+)
+
+    //import {ref} from 'vue'
+
+    //const timer=ref(0)
+    //setInterval(()=>timer.value++,1000)
+</script>
+
+<style scoped>
+    .success{
+        background-color:green;
+        color:white;
+    }
+</style>
